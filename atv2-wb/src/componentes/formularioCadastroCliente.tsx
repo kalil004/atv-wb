bdbc332011b1f25da0b02adb7fcc0fb41372f122
Kalil Pereira <kalil.alves.2019@gmail.com>
2024-06-10 07:36:13 -0300

    Update formularioCadastroCliente.tsx

--- atv2-wb/src/componentes/formularioCadastroCliente.tsx
+++ atv2-wb/src/componentes/formularioCadastroCliente.tsx
@@ -14,26 +14,26 @@ export default class FormularioCadastroCliente extends Component<props> {
                     <div className="row">
                         <div className="input-field col s6">
                             <input id="first_name" type="text" className="validate" />
-                            <label htmlFor="first_name">nome</label>
+                            <label htmlFor="first_name">Nome</label>
                         </div>
                         <div className="input-field col s6">
                             <input id="last_name" type="text" className="validate" />
-                            <label htmlFor="last_name">sobrenome</label>
+                            <label htmlFor="last_name">Sobrenome</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
                             <input id="telefone" type="text" className="validate" />
-                            <label htmlFor="telefone">telefone</label>
+                            <label htmlFor="telefone">Telefone</label>
                         </div>
                         <div className="input-field col s6">
                             <input id="email" type="email" className="validate" />
-                            <label htmlFor="email">e-mail</label>
+                            <label htmlFor="email">E-mail</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col s12">
-                            <button className={estiloBotao} type="submit" name="action">Submit
+                            <button className={estiloBotao} type="submit" name="action">Cadastrar
                                 <i className="material-icons right">send</i>
                             </button>
                         </div>
